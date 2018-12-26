12d0b4c9538f8e8683a50428ca42948c78c2eaf1
Raphael do Vale <raphael@dovale.net>
2018-12-26 01:12:54 -0200

    Services now connect with Eureka

--- school-ui/src/main/java/com/okta/developer/microservicedockerspring/ui/controller/SchoolController.java
+++ school-ui/src/main/java/com/okta/developer/microservicedockerspring/ui/controller/SchoolController.java
@@ -1,45 +1,32 @@
 package com.okta.developer.microservicedockerspring.ui.controller;
 
-
 import com.okta.developer.microservicedockerspring.ui.dto.TeachingClassDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class SchoolController {
-
     private final RestTemplate restTemplate;
-
-    private final String serviceHost;
-
     @Autowired
-    public SchoolController(RestTemplate restTemplate, @Value("${service.host}") String serviceHost) {
+    public SchoolController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.serviceHost = serviceHost;
     }
-
     @RequestMapping("")
     public ModelAndView index(){
         return new ModelAndView("index");
     }
-
     @GetMapping("/classes")
     public ResponseEntity<List<TeachingClassDto>> listClasses(){
 
         return restTemplate
-                .exchange("http://"+ serviceHost +"/class", HttpMethod.GET, null,
+                .exchange("http://school-service/class", HttpMethod.GET, null,
                         new ParameterizedTypeReference<List<TeachingClassDto>>() {});
-
     }
 }
